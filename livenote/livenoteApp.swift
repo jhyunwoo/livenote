7de1a3c5c8e5abc2d50780cddad1285fcadc06ca
jhyunwoo <jhyunwoo0228@gmail.com>
2026-01-26 15:42:59 +0900

    Initial Commit

---
+++ livenote/livenoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  livenoteApp.swift
+//  livenote
+//
+//  Created by 전현우 on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct livenoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
